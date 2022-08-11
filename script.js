33894b63f203877cbc07ced60d3e1145869da1fd
Dark-WizZ <mailwithakram@gmail.com>
2022-08-12 05:02:29 +0530

    Add active password validation

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+const password = document.querySelector('#password');
+const cpassword = document.querySelector('#cpassword');
+const passwordRules = document.querySelector('.password-rules');
+const uppercase = document.querySelector('#uppercase');
+const lowercase = document.querySelector('#lowercase');
+const number = document.querySelector('#number');
+const digit = document.querySelector('#digit');
+const validColor = '#29F8FE';
+const invalidColor = '#B80692';
+
+cpassword.addEventListener('keyup', verifyPassword);
+password.addEventListener('focus', passwordFocus);
+password.addEventListener('focusout', passwordFocusOut);
+password.addEventListener('keyup', passwordKeyUp);
+
+function passwordKeyUp(){
+  uppercase.style.color = this.value.match('[A-Z]')? validColor: invalidColor;
+  lowercase.style.color = this.value.match('[a-z]')? validColor: invalidColor;
+  number.style.color = this.value.match('[0-9]')? validColor: invalidColor;
+  digit.style.color = (this.value.length>=8)? validColor: invalidColor;
+}
+
 function verifyPassword(){
   if(password.value!=cpassword.value){
     this.setCustomValidity('Invalid field.')
@@ -6,7 +28,10 @@ function verifyPassword(){
   }
 }
 
-const password = document.querySelector('#password');
-const cpassword = document.querySelector('#cpassword');
+function passwordFocus(){
+  passwordRules.style.display = 'block';
+}
 
-cpassword.addEventListener('keyup', verifyPassword)
+function passwordFocusOut(){
+  passwordRules.style.display = 'none';
+}
